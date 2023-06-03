9fdc6b8f767c2c5eacf890e099cdc0a6a7dd3e8a
Amit Malur <amitmalur@gmail.com>
2023-06-03 17:16:17 +0530

    added contact page and send email code

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
 st.header("Contact Us")
+
+with st.form(key="myForm"):
+    user_name = st.text_input("Your Email Address")
+    message = st.text_area("Your Message Here")
+    button = st.form_submit_button("Submit")
+    if button:
+        message = message + user_name
+        print("button clicked")
